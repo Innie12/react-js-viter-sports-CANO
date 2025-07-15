42ec2af38a5d54ded662398fe03c824083f061f6
bullets012 <lancegenesismercado0111@gmail.com>
2025-07-15 12:09:05 +0800

    commit

--- src/components/partials/PageNotFound.jsx
+++ src/components/partials/PageNotFound.jsx
@@ -5,16 +5,16 @@ const PageNotFound = () => {
   return (
     <>
       <div className="container mx-auto p-10 text-center mt-60 bg-blue-100 ">
-        <div className="bg-blue-50 shadow-2xl p-14">
+        <div className="bg-blue-50 shadow-2xl p-10">
           <p className="text-7xl mb-5">⚠️</p>
           <h1 className="text-blue-700 font-bold text-6xl ">
-            404 - Page Not Found
+            4<span className="text-orange-700">0</span>4 - Page Not Found
           </h1>
           <Link
             to="/"
             className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 inline-block mt-14 "
           >
-            Go Back to Home Page
+            Return Home
           </Link>
         </div>
       </div>
