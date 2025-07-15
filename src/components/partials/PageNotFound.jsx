19314aed6dc22f3a3b835c54ff0711cc268f58c7
bullets012 <lancegenesismercado0111@gmail.com>
2025-07-15 11:42:05 +0800

    commit

--- src/components/partials/PageNotFound.jsx
+++ src/components/partials/PageNotFound.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 const PageNotFound = () => {
   return (
     <>
-      <div className="container mx-auto p-10 text-center mt-72">
-        <h1 className="text-blue-700 font-bold text-7xl ">
+      <div className="container mx-auto p-10 text-center mt-60">
+        <p className="text-7xl mb-5">⚠️</p>
+        <h1 className="text-blue-700 font-bold text-6xl ">
           404 - Page Not Found
         </h1>
         <Link
           to="/"
-          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 inline-block mt-8 "
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 inline-block mt-14 "
         >
-          Go Back to Home
+          Go Back to Home Page
         </Link>
       </div>
     </>
